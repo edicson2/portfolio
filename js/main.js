210a84ecde3a3bd2eba1035d92cf351d1f049aed
EG <edicson.garcia.luis@umontreal.ca>
2021-05-25 13:45:39 -0400

    Updates

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
-// Squares random initial position
-const boxes = document.getElementById('box').children;
-for (const box of boxes) {
-  let rnd = Math.random() * (90 - 1) + 1;
-  // console.log(rnd);
-  box.style.setProperty('left', rnd + '%');
-  rnd = Math.random() * (90 - 10) + 10;
-  box.style.setProperty('top', rnd + '%');
-  rnd = Math.random() * (8 - 3) + 3;
-  box.style.setProperty('time', rnd + 's');
+const randomSquares = () => {
+  const boxes = document.getElementById('box').children;
+  for (const box of boxes) {
+    let rnd = Math.random() * (90 - 1) + 1;
+      // console.log(rnd);
+    box.style.setProperty('left', rnd + '%');
+    rnd = Math.random() * (90 - 10) + 10;
+    box.style.setProperty('top', rnd + '%');
+    rnd = Math.random() * (8 - 3) + 3;
+    box.style.setProperty('time', rnd + 's');
+  }
+  setTimeout(randomSquares, rand * 1000);
 }
+
+// Squares random initial position
+randomSquares();
